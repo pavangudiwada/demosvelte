ba0f4c2e567983e7f3f1ee93acf4eaa8f90c1d75
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2025-01-09 08:35:14 +0530

    Svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // import HeroSection from "$lib/components/HeroSection.svelte";
+    import {
+        HeroSection,
+        ChapterPreview,
+        AuthorSection,
+        FaqSection,
+    } from "$components";
+</script>
+
+<HeroSection />
+<ChapterPreview />
+<AuthorSection />
+<FaqSection />
